703261d48de4c0bbaaf14ae2889ae819882e434c
Noah Evans <noah00106@gmail.com>
2022-03-02 18:34:22 -0500

    Completed step two

--- evans_noah_python2/codeforsteptwo.py
+++ evans_noah_python2/codeforsteptwo.py
@@ -1,3 +1,4 @@
+import sqlalchemy
 from flask import Flask, render_template, url_for, redirect, session
 import os
 from flask_sqlalchemy import SQLAlchemy
@@ -5,8 +6,16 @@ from flask_migrate import Migrate
 import pymysql
 from formtest import AddOwner, AddCanine
 from mainforapp import db, Puppy, Owner
+import pandas as pd
+import numpy as np
 
 
 
-db.session.add(my_puppy)
-db.session.commit()
+#I used https://www.youtube.com/watch?v=lNL4sCa3otU for assistance with migrating data from DF to MySQL, also used DF documentation: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_sql.html
+engine = sqlalchemy.create_engine('mysql+pymysql://root:Password1!@localhost/Appdata1')
+ownerframe = pd.read_csv(r'C:\Users\tveva\PycharmProjects\tek-evans-python2\evans_noah_python2\csvs for step two\ownertable.csv', index_col=False)
+puppyframe = pd.read_csv(r'C:\Users\tveva\PycharmProjects\tek-evans-python2\evans_noah_python2\csvs for step two\puppytable.csv', index_col=False)
+print(ownerframe)
+print(puppyframe)
+ownerframe.to_sql('owner', engine, schema='appdata1', if_exists='append', index=False)
+puppyframe.to_sql('puppies', engine, schema='appdata1', if_exists='append', index=False)
